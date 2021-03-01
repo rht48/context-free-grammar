8d509ffb8a37260b75c5dc39b7e1ad2201d19f87
rht48 <romain.hubert48@yahoo.com>
2021-03-01 12:01:21 +0100

    Add modal

--- src/app/pages/home/config-grammar/config-grammar.component.ts
+++ src/app/pages/home/config-grammar/config-grammar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-config-grammar',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigGrammarComponent implements OnInit {
 
-  constructor() { }
+  // Used for showing the modal
+  modalRef!: BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
   }
 
+  openModal(template: TemplateRef<any>): void {
+    // Show the modal
+    this.modalRef = this.modalService.show(template);
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,11 +4,15 @@ import { HomeRoutingModule } from './home.routing.module';
 import { HomePage } from './home.page';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ConfigGrammarComponent } from './config-grammar/config-grammar.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
+    ModalModule.forRoot(),
+    BsDropdownModule.forRoot(),
     FormsModule,
     ReactiveFormsModule
   ],
